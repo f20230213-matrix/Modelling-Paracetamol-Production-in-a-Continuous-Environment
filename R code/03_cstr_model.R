3dc55f6c8d264de8cb81f48c2bacb7a5bda03a07
f20230213-matrix <f20230213@pilani.bits-pilani.ac.in>
2026-09-23 17:35:34 +0530

    Update 03_cstr_model.R

--- R code/03_cstr_model.R
+++ R code/03_cstr_model.R
@@ -12,7 +12,7 @@
 #   V*dCi/dt = Q*(Ci_feed - Ci) + V*ri
 # =============================================================================
 
-source("C:/Users/inbox/Downloads/paracetamol_reactor_study/paracetamol_project/R/01_parameters.R")
+source("R code/01_parameters.R")
 library(deSolve)
 
 # ---------- Steady-state (algebraic solve via iteration) ----------
